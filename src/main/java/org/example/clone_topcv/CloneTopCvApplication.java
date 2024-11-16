e80810a006b24b69df2a08e522841069c179f347
Phong Bui <phongbuibsp1@gmail.com>
2024-11-16 11:12:54 +0700

    test

---
+++ src/main/java/org/example/clone_topcv/CloneTopCvApplication.java
@@ -0,0 +1,13 @@
+package org.example.clone_topcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneTopCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneTopCvApplication.class, args);
+    }
+
+}
